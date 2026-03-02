b06969f3762c05c584729f9173f1de439e2b6f63
aq8u8pa <aq8u8pa@gmail.com>
2026-03-02 16:20:00 +0900

    feat: add flask codes for error handling

--- 02-flask/flask/app/__init__.py
+++ 02-flask/flask/app/__init__.py
@@ -4,4 +4,20 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+from app import routes, model, errors
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
